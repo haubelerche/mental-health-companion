857735a7f9171b72d97f7fe30f1c3d61b5d3739d
eltad2003 <eltad2003@gmail.com>
2026-04-18 17:19:36 +0700

    feat: add wave breath icon with lucide react

--- frontend/src/components/pages/landing/BreathSection.tsx
+++ frontend/src/components/pages/landing/BreathSection.tsx
@@ -1,17 +1,19 @@
+import { Waves } from "lucide-react";
+
 export default function BreathSection() {
     return (
         <section className="px-6 py-30" id="breath-space">
-            <div className="max-w-4xl mx-auto text-center space-y-20 px-6">
+            <div className="max-w-4xl mx-auto text-center space-y-15 px-6">
                 <div className="space-y-4">
                     <h2 className="text-6xl font-display text-white italic">Hãy để tâm trí được nghỉ ngơi</h2>
                     <p className=" text-white/70 text-lg">Hít thở sâu theo nhịp điệu của đại dương</p>
                 </div>
                 <div className="flex justify-center">
                     <div className="relative flex items-center justify-center">
-                        <div className="w-80 h-80 rounded-full border border-white/10 animate-[ping_4s_linear_infinite] opacity-20"></div>
-                        <div className="absolute w-64 h-64 rounded-full glass-panel border border-white/20 flex items-center justify-center editorial-shadow">
-                            <div className="w-40 h-40 rounded-full bg-white/10 backdrop-blur-xl border border-white/30 flex items-center justify-center">
-                                <span className="material-symbols-outlined text-white text-5xl animate-pulse">waves</span>
+                        <div className="w-50 h-50 rounded-full border border-gray animate-[ping_2s_linear_infinite] opacity-30"></div>
+                        <div className="absolute w-50 h-50 rounded-full border border-white/20 flex items-center justify-center bg-white/20 backdrop-blur-xl">
+                            <div className="w-30 h-30 rounded-full bg-white/10 backdrop-blur-xl border border-white/10 flex items-center justify-center hover:bg-white/20 transition hover:cursor-pointer">
+                                <Waves size={50}/>
                             </div>
                         </div>
                     </div>
