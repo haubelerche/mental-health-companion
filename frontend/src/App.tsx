5e3b8e599ed52bdfc9ce49b9ff4fc67f382620b3
eltad2003 <eltad2003@gmail.com>
2026-04-28 13:25:29 +0700

    feat: toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter } from 'react-router-dom'
 import ScrollToHash from './utils/ScrollToHash'
 import AppRoutes from './routes/AppRoutes'
+import { ToastContainer } from 'react-toastify'
 
 
 
@@ -10,6 +11,17 @@ export default function App() {
     <BrowserRouter>
       <ScrollToHash />
       <AppRoutes />
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        pauseOnFocusLoss
+        pauseOnHover
+        draggable
+        theme="light"
+      />
     </BrowserRouter>
   )
 }
